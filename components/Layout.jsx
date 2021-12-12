6c944b4418174ec2aa5b5b369855e0661083ec37
kazuki827 <xm.kazuki827@gmail.com>
2021-12-12 16:43:46 +0900

    modify logo position

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children, title = "Kazuki's Blog" }) => {
         <title>{title}</title>
       </Head>
       <nav className="w-screen flex items-center justify-between flex-wrap bg-gray-800 p-3">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <div className="flex items-center flex-shrink-0 text-white px-6">
           <Link href="/">
             <a className="font-semibold text-xl text-gray-200 hover:opacity-80">
               Kazuki&apos;s Blog
